ef6e9fa69aa652683bf26bae85bfcf30f903d5d6
Cody King <122841869+Cjking57893@users.noreply.github.com>
2024-10-23 01:10:41 -0400

    Minimizing to button on side of screen working

--- main.js
+++ main.js
@@ -1,32 +1,78 @@
-const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+
+// Require electron module to reload when file changes saved.
+require('electron-reload')(path.join(__dirname));
+
 
 let mainWindow;
+let miniButtonWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
-  })
+  });
 
   mainWindow.loadFile('index.html')
   mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
+
+// IPC listener to minimize the window.
+ipcMain.on('minimize-to-button', (e) => {
+    // Hide the main window.
+    mainWindow.hide();
+
+    // Creating mini window for button.
+    miniButtonWindow = new BrowserWindow({
+      width: 70,
+      height: 70,
+      x: 0,
+      y: 100,
+      frame: false,
+      transparent: true,
+      alwaysOnTop: true,
+      resizable: false,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+      }
+    });
+
+    miniButtonWindow.loadFile('mini-button.html');
+
+    // Closing mini window when clicking button and restoring the main window.
+    miniButtonWindow.on('closed', () => {
+        mainWindow.show();
+        miniButtonWindow = null;
+    })
+});
+
+// IPC listener to restore the main window.
+ipcMain.on('restore-main-window', () => {
+    if (mainWindow) {
+      mainWindow.show();
+      miniButtonWindow.close();
+    }
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
